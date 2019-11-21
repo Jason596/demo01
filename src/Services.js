9341bf7cfd50fd1d6f6e7481b63000a3dec70b1f
Jason596 <zwu047@hotmail.com>
2019-11-21 22:07:25 +1300

    jsx, component, fragment, export compoent.

---
+++ src/Services.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from "react";
+
+class Services extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <input />
+          <button>Enter new service</button>
+        </div>
+        <ul>
+            <li>Head massage</li>
+            <li>Back massage</li>
+        </ul>
+      </Fragment>
+    )
+  }
+}
+
+
+export default Services 
